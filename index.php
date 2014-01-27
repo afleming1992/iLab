3da97ca127c30a3635fd0d3e496b20cef4c02f67
Andrew Fleming <ajf9@hw.ac.uk>
2014-01-27 12:45:30 +0000

    Initial Commit and Setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Heriot-Watt Interaction Lab</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body> 
+    <div class='wrapper'>
+        <p>Blah</p>
+    </div>
+</body>
+</html>
