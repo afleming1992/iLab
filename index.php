08a62ae639051da502ee03c687eec3388cd19c18
Andrew Fleming <ajf9@hw.ac.uk>
2014-01-29 09:19:20 +0000

    Overhalled Look of Website and begining structuring

--- index.php
+++ index.php
@@ -3,13 +3,29 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Heriot-Watt Interaction Lab</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body> 
     <div class='wrapper'>
-        <header>
-            Testing
-        </header>
+      <header>
+        <?php
+          include('navbar.php')
+        ?>
+      </header>
+      <div class='maincontent'>
+        <div style='float:left'>
+          
+        </div>
+        <div style='float:right'>
+          <div class='row'>
+
+          </div>
+        </div>
+      </div>
+      <footer>
+        <b>Footer</b>
+      </footer>
     </div>
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,21 @@
+<nav class="mainnav navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+    <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+      <span class='sr-only'>Toggle Navigation</span>
+      <span class='icon-bar'></span>
+      <span class='icon-bar'></span>
+      <span class='icon-bar'></span>
+    </button>
+    <a class='navbar-brand' href='#'><img src='images/logo.png' height='55px'/></a>
+  </div>
+    <ul class='nav navbar-nav'>
+      <li><a href='#'>Home</a></li>
+      <li><a href='#'>About Us</a></li>
+      <li><a href='#'>Research</a></li>
+      <li><a href='#'>Publications</a></li>
+      <li><a href='#'>Intranet</a></li>
+    </ul>
+    <button id='navbar_profile' type="button" class="btn btn-primary dropdown-toggle" style='float:right;'>
+      Login <span class='glyphicon glyphicon-lock'></span> 
+    </button>
+</nav>
